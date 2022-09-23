a3baec2cccf58edbd195b1d041998d88b5f8523e
Daniel Suarez <suarez.daniel101@gmail.com>
2022-09-23 09:26:11 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 10 }
   validates :summary, length: { maximum: 299 }
-  validates  inclusion: { in: %w("") }
+  validates :category, inclusion: { in: %w[Fiction Non-Fiction] }
 end
