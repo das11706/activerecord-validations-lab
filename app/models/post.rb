02a02e99518a002de3487cbb17b0be1802544689
Daniel Suarez <suarez.daniel101@gmail.com>
2022-09-23 09:20:40 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 10 }
   validates :summary, length: { maximum: 299 }
-  validates :category, inclusion: { in: %w("") }
+  validates  inclusion: { in: %w("") }
 end
