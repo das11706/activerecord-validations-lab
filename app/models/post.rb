6eacafa3a15e37c07783653287c6796733e7c612
Daniel Suarez <suarez.daniel101@gmail.com>
2022-09-27 16:35:30 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,12 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 10 }
   validates :summary, length: { maximum: 299 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :non_clickbait
+
+  def non_clickbait
+    if title != ('Won\'t Believe' || 'Secret' || 'Top [number]' || 'Guess')
+      errors.add(:title, "Won\'t Believe or Secret or Top[number] or Guess")
+    end
+  end
+
 end
