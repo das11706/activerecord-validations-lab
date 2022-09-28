4542e44e47ad91961d751bbcf5a7d4724cda08dc
Daniel Suarez <suarez.daniel101@gmail.com>
2022-09-28 15:50:08 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ActiveRecord::Base
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
   validate :non_clickbait
 
-  TITLE = [ 
+  Title = [ 
     /Won't Believe/i, 
     /Secret/i,
     /Top [0-9]*/i,
@@ -13,17 +13,20 @@ class Post < ActiveRecord::Base
   ]
 
   def non_clickbait
-    if TITLE.none? {|pattern| pattern.match title}
-      errors.add(:title, "Won\'t Believe or Secret or Top[number] or Guess")
+    if Title.none? do |pattern|
+        pattern.match(title)
+    end
+      errors.add(:title, "Won\'t Believe || Secret || Top[number] || Guess")
     end
   end
 
+  
 #   def non_clickbait
-#     if title != ('Won\'t Believe' || 'Secret' || 'Top [number]' || 'Guess')
-#     # unless title.include?('Won\'t Believe' || 'Secret' || 'Top [number]' || 'Guess') 
+#     if title != (/Won\'t Believe/i || /Secret/i || /Top [0-9]*/i || /Guess/i)
+#     # unless title.include?(/Won\'t Believe/i || /Secret/i || /Top [0-9]*/i || /Guess/i)
 #     # validates :title, inclusion: { in: %w('Won\'t Believe' 'Secret' 'Top [number]' 'Guess') }
 #     # validates :title, presence: true
-#       errors.add(:title, "Won\'t Believe or Secret or Top[number] or Guess")
+#       errors.add(:title, "Won\'t Believe || Secret || Top[number] || Guess")
 #     end
 #   end
 
